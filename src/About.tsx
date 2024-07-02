2ac9055447a055c345ccab8d858382f372376b70
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-07-02 11:58:06 +0300

    feat: add Home router with About and Projects components

---
+++ src/About.tsx
@@ -0,0 +1,36 @@
+export default function About() {
+	return (
+		<div id="about-container">
+			<div id="skills">
+				<h1>Tech Stack</h1>
+				<ul>
+					<li>Golang</li>
+					<li>PostgreSQL</li>
+					<li>Docker</li>
+					<li>Linux</li>
+					<li>gRPC</li>
+					<li>REST</li>
+					<li>Redis</li>
+					<li>Git</li>
+				</ul>
+			</div>
+
+			<div id="bio">
+				<h1>Bio</h1>
+				<p>
+					Hi! I'm a student at Innopolis University. Currently learning backend
+					development in Golang. I found this language quite convenient and it
+					is popular.
+				</p>
+
+				<p>
+					I have no projects worth to show here. I still cover the basics of
+					programming jumping from one technology to another without completing
+					any project
+				</p>
+
+				<p>In free time I play guitar, bake, scateboarding.</p>
+			</div>
+		</div>
+	);
+}

6d706f157856309e9a318e28fd0eb98ea8887721
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-07-02 11:57:03 +0300

    feat: create react-router setup for site

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+import Blog from "./Blog";
+import Comic from "./Comic";
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: "blog",
+				element: <Blog />,
+			},
+			{
+				path: "comic",
+				element: <Comic />,
+			},
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 );

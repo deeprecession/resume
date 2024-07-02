97058ebc420a6974f342b3fedf6788a9fac9659a
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-07-02 13:18:37 +0300

    fix: change createBrowderRouter to createHashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./Home";
 import Blog from "./Blog";
 import Comic from "./Comic";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
 	{
 		path: "/",
 		element: <App />,

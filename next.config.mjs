dc3bbf1846a5840325f7975b947d0e1fd1bdc0db
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-07-04 12:37:08 +0300

    feat: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: "./dist",
+    reactStrictMode: true,
+
+    trailingSlash: true,
+
+    output: "export",
 
     basePath: "/inno-frontend-course",
+    assetPrefix: "/inno-frontend-course",
+
+    distDir: "./dist",
+
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+                port: "",
+                pathname: "**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
